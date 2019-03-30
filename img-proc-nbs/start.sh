39e08db79196f2cdb61e886c0f9bfb9c5d344709
Luis Gomes <gomes.luis479@gmail.com>
2019-03-31 01:38:11 +0530

    expt_3

--- img-proc-nbs/start.sh
+++ img-proc-nbs/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source /home/luis/python-envs/img-proc-1/bin/activate
-jupyter notebook --notebook-dir=/home/luis/Desktop/workspace/image-processing/img-proc-nbs
+jupyter notebook
 
 
